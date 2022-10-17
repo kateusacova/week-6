a1fa1564faf19116b96142a4fab26e1a28decf8b
kateusacova <kateusacova@gmail.com>
2022-10-17 11:48:37 +0100

    Exercise 1.4

---
+++ add.js
@@ -0,0 +1,5 @@
+const add = (num_1, num_2) => {
+  return num_1 + num_2;
+}
+
+module.exports = add;
---
+++ multiply.js
@@ -0,0 +1,5 @@
+const multiply = (num_1, num_2) => {
+  return num_1 * num_2;
+}
+
+module.exports = multiply;
